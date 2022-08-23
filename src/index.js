eeaa5ee02fb8b3d6b1ea241610fff149e4d3d7d5
Jose Kupeka Muhlanga <jose.muhlanga@student.moringaschool.com>
2022-08-23 20:05:44 +0300

    Attempted on Aug 23

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleTask(e.target.new-task-description.value)
+  })
 });
+
+function handleTask(task){
+  let p = document.createElement('li');
+  let btn =document.createElement('button');
+  btn.textContent = 'x';
+  p.textContent = task;
+  document.querySelector('#tasks').appendChild(p)
+}
